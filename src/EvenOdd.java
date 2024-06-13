fb7a14da1b83047c714d7a1bfdf2ddf1c270f560
morritt99 <morritt@mail.uc.edu>
2024-06-12 20:32:33 -0400

    Commit

---
+++ src/EvenOdd.java
@@ -0,0 +1,13 @@
+
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int numToExamine = 2;
+        if (numToExamine % 2 == 0) {
+            System.out.println(numToExamine + "is Even");
+
+        } else {
+            System.out.println(numToExamine + "is Odd");
+        }
+    }
+}
